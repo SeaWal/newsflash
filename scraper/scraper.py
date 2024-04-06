003b76d107005f1b138e439b5c38b5152f142632
SeaWal <sean.walshe@tutanota.com>
2024-04-06 14:49:46 +0100

    rename __load_source to __load_sources and add impl to open json file and store in instance var

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,12 +4,23 @@ from pathlib import Path
 class WebScraper:
     def __init__(self, source_file: Path) -> None:
         self.source_file = source_file
+        self.__load_sources()
 
     def scrape(self) -> None:
         pass
 
-    def __load_source(self) -> None:
-        pass
+    def print_sources(self) -> None:
+        for source in self.sources:
+            print(f"Source: {source['source']}, Url: {source['url']}")
+
+    def __load_sources(self) -> None:
+        try:
+            with open(self.source_file, 'r') as f:
+                self.sources = json.load(f)['sources']
+        except FileNotFoundError:
+            print(f"Couldn't find file {self.source_file}.")
+        except json.JSONDecodeError:
+            print(f"Couldn't decode {self.source_file} as JSON.")
 
     def __fetch_page(self) -> None:
         pass
@@ -19,3 +30,9 @@ class WebScraper:
 
     def __fetch_and_parse(self) -> None:
         pass
+
+
+if __name__ == '__main__':
+    path = Path(__file__).parent.parent / "sources.json"
+    scraper = WebScraper(path)
+    scraper.print_sources()
