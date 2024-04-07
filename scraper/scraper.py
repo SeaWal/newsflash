cae34092a89acff7e56169f48102045e0c6004df
SeaWal <sean.walshe@tutanota.com>
2024-04-07 17:13:18 +0100

    add __parse_selector method, and switch order of for-loop and try-catch in __parse_page

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -15,7 +15,8 @@ class WebScraper:
     async def scrape(self) -> List[Article]:
         try:
             tasks = [self.__fetch_and_parse(source) for source in self.sources]
-            results = await asyncio.gather(*tasks)
+            results =  await asyncio.gather(*tasks)
+            return [article for article_list in results for article in article_list]
         except Exception as e:
             print(f"Exception occurred during scraping.")
 
@@ -33,20 +34,27 @@ class WebScraper:
             print(f"Couldn't decode {self.source_file} as JSON.")
 
     async def __fetch_page(self, url: str) -> bytes:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                return await response.read()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    return await response.read()
+        except:
+            print(f"Error while fetching page {url}.")
 
     async def __parse_page(self, source, html: bytes) -> List[Article]:
         soup = BeautifulSoup(html, 'html.parser')
         articles = []
         parse_rules = source['parse_rules']
-        for article_html in article_html.select(parse_rules['article_selector']):
-            title = article_html.select_one(parse_rules['title_selector'])
-            href = article_html.select_one(parse_rules['href_selector'])
-            summary = article_html.select_one(parse_rules['summary_selector'])
-            article = Article(title, href, summary)
-            articles.append(article)
+        for article_html in soup.select(parse_rules['article_selector']):
+            try:
+                title = await self.__parse_selector(article_html, parse_rules['title_selector'])
+                href = await self.__parse_selector(article_html, parse_rules['href_selector'])
+                summary = await self.__parse_selector(article_html, parse_rules['summary_selector'])
+                article = Article(title.contents[0], href['href'], summary.contents[0])
+                articles.append(article)
+            except Exception as e:
+                print(f"Error while parsing page {source['url']}. {e}")
+                continue
         return articles
     
     async def __fetch_and_parse(self, source) -> List[Article]:
@@ -54,7 +62,14 @@ class WebScraper:
             html = await self.__fetch_page(source['url'])
             return await self.__parse_page(source, html)
         except Exception as e:
-            print(f"An error occurred while fetching and parsing {source['name']}.")
+            print(f"An error occurred while fetching and parsing {source['name']}. {e}")
+
+    async def __parse_selector(self, html: bytes, selector: str) -> list | dict:
+        try:
+            return html.select_one(selector)
+        except:
+            print(f"An error occurred while attempting to parse {selector}")
+            return []
 
 if __name__ == '__main__':
     path = Path(__file__).parent.parent / "sources.json"
