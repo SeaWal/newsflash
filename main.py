0604cab2791d2c99b6f3b9c8a4d414496342874d
SeaWal <sean.walshe@tutanota.com>
2024-04-28 16:37:01 +0100

    set app.articles to result of webscraping

--- main.py
+++ main.py
@@ -8,10 +8,8 @@ async def main():
     path = Path(__file__).parent / "sources.json"
     scraper = WebScraper(path)
     results = await scraper.scrape()
-    for result in results:
-        print(result)
-        print("="*20)
-
+    
+    app.articles = results
     app.run()
 
 if __name__ == '__main__':
