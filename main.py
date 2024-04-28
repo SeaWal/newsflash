38c9c5d0f6fb836d5ddfea1500bfab8d20b439bc
SeaWal <sean.walshe@tutanota.com>
2024-04-28 15:46:49 +0100

    adding flask backend, and test hello_world

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import asyncio
 
 from scraper import WebScraper
-
+from web import app
 
 async def main():
     path = Path(__file__).parent / "sources.json"
@@ -11,6 +11,8 @@ async def main():
     for result in results:
         print(result)
         print("="*20)
+
+    app.run()
 
 if __name__ == '__main__':
     asyncio.run(main())
